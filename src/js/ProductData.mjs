72555947b317357dfa40b95b12d76a4c76c83c32
Mauricio Reyes <maureyes.dev@gmail.com>
2025-09-16 16:39:44 -0600

    feat: temporary fix to fetch the json based on github url

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -9,7 +9,9 @@ function convertToJson(res) {
 export default class ProductData {
   constructor(category) {
     this.category = category;
-    this.path = `${location.origin}/json/${this.category}.json`;
+    // this.path = `${location.origin}/json/${this.category}.json`;
+    // HACK: Temporary fix for loading JSON from GitHub
+    this.path = `https://raw.githubusercontent.com/reyesmarq/wdd330-sleepoutside/refs/heads/main/src/json/${this.category}.json`;
   }
 
   getData() {
