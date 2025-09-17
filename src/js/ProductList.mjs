9d2b558d262180a980bd5c757a612e040d4a4e59
Marcela <lmvt.07@gmail.com>
2025-09-17 16:03:40 -0600

    Add initial tents data JSON file with multiple tent entries and details

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,35 +1,33 @@
-import { renderListWithTemplate } from "./utils.mjs";
-
 
 function productCardTemplate(product) {
+    console.log("product===========", product)
+  const discount = Math.round(product.SuggestedRetailPrice - product.FinalPrice);
   return `
   <li class="product-card">
     <a href="product_pages/?product=${product.Id}">
       <img src="${product.Image}" alt="${product.Name}">
       <h2 class="card__brand">${product.Brand.Name}</h2>
       <h3 class="card__name">${product.Name}</h3>
-      <p class="product-card__price">$ ${product.FinalPrice}</p>
+      <p class="product-card__price">$ ${product.FinalPrice} ${product.FinalPrice < product.SuggestedRetailPrice ? '<span class="discount-badge">¡Sale!</span>' : ''}</p>
+      <span class="product-card__price">$ ${discount}.00 off</span>
     </a>
-  </li>`
+  </li>`;
 }
 
 export default class ProductList {
+  constructor(category, dataSource, listElement) {
+    this.category = category;
+    this.dataSource = dataSource;
+    this.listElement = listElement;
+  }
 
-    constructor(category, dataSource, listElement){
-        this.category = category
-        this.dataSource = dataSource
-        this.listElement = listElement
-    }
-
-    async init(){
+  async init() {
     const list = await this.dataSource.getData();
-    this.renderList(list)
-    }
+    this.renderList(list);
+  }
 
-    renderList(list){
-    // const htmlstring = product.map(productCardTemplate);
-    // this.listElement.insertAdjacentHTML('afterbegin', htmlstring.join(''));
-
-     renderListWithTemplate(productCardTemplate, this.listElement, list);
-}
+  renderList(list) {
+    const htmlstring = list.map(productCardTemplate);
+    this.listElement.innerHTML = htmlstring.join('');
+  }
 }
--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,9 @@
 import ProductData from './ProductData.mjs'
 import ProductList from './ProductList.mjs'
 
-const productSource = new ProductData("tents")
-
+const dataSource = new ProductData("tents")
 const element = document.querySelector(".product-list")
 
-const productList = new ProductList("tents", productSource, element)
-
+const productList = new ProductList("tents", dataSource, element)
 productList.init()
 
