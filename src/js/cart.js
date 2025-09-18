b4e7af730b9832c6eb5cad8e2e0af23c2f5a9d34
Mauricio Reyes <maureyes.dev@gmail.com>
2025-09-18 16:09:54 -0600

    feat: add support for remove item from cart and empty cart was already fixed

--- src/js/cart.js
+++ src/js/cart.js
@@ -6,23 +6,45 @@ function renderCartContents() {
   document.querySelector('.product-list').innerHTML = htmlItems.join('');
 }
 
+/**@param {import("../types/product.d.ts").Product} item
+ * @returns {string} HTML string
+ */
 function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
+  const newItem = `
+    <li class="cart-card divider">
+      <a href="#" class="cart-card__image">
+        <img
+          src="${item.Image}"
+          alt="${item.Name}"
+        />
+      </a>
+      <a href="#">
+        <h2 class="card__name">${item.Name}</h2>
+      </a>
+      <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+      <p class="cart-card__quantity">qty: 1</p>
+      <p class="cart-card__price">$${item.FinalPrice}</p>
+      <button class="cart-card__delete-btn" data-id=${item.Id} id="deleteFromCart">Delete</button>
+    </li>
+  `;
 
   return newItem;
 }
 
 renderCartContents();
+
+document.querySelector('.product-list').addEventListener('click', (e) => {
+  if (e.target && e.target.id === 'deleteFromCart') {
+    console.log('click', e.target.dataset.id);
+    const cartItems = getLocalStorage('so-cart') ?? [];
+    const itemToRemoveIndex = cartItems.findIndex(
+      (item) => item.Id === e.target.dataset.id,
+    );
+    if (itemToRemoveIndex > -1) {
+      cartItems.splice(itemToRemoveIndex, 1);
+      localStorage.setItem('so-cart', JSON.stringify(cartItems));
+      renderCartContents();
+    }
+  }
+});
+console.log('🚀 ~ element', element);
