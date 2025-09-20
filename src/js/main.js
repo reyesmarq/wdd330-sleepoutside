de58a4e313e2b9c89482ecfa463f854aa22a02d0
LivaJhonson <livajhonson69@gmail.com>
2025-09-20 17:32:37 +0300

    new file:   src/js/Alert.mjs 	modified:   src/js/main.js 	new file:   src/public/json/alerts.json

--- src/js/main.js
+++ src/js/main.js
@@ -7,3 +7,13 @@ const element = document.querySelector(".product-list")
 const productList = new ProductList("tents", dataSource, element)
 productList.init()
 
+// main.js
+// ... (existing imports like ProductData and ProductList)
+import Alert from './Alert.mjs';
+
+// ... (existing code to set up your product list)
+
+// Instantiate the Alert class and call its init method.
+const mainElement = document.querySelector('main');
+const alerts = new Alert(mainElement);
+alerts.init();
